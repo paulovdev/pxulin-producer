497b504f952b1283efa800fe67aad1d49077e328
pxulin <fudevil123@gmail.com>
2023-08-19 16:22:34 -0300

    new

--- src/pages/music/Music.jsx
+++ src/pages/music/Music.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import './Music.scss';
-import { Fade } from 'react-awesome-reveal';
-
+import React from "react";
+import "./Music.scss";
+import { Fade } from "react-awesome-reveal";
 
 const Music = () => {
-
-    return (
-        <section id='music'>
-            <main className="grid-layout">
-                <div className="music-content">
-                    <Fade className='animation' cascade direction='down' duration={400} triggerOnce>
-                        <h1>MUSIC</h1>
-                        <p>
-                            SOON
-                        </p>
-                    </Fade>
-                </div>
-                <div className="slider">
-                    <h1>SOON SOON SOON SOON SOON SOON</h1>
-                    <h1>SOON SOON SOON SOON SOON SOON</h1>
-                </div>
-            </main>
-        </section>
-    );
+  return (
+    <section id="music">
+      <main className="grid-layout">
+        <div className="music-content">
+          <Fade
+            className="animation"
+            cascade
+            direction="down"
+            duration={400}
+            triggerOnce
+          >
+            <h1>MUSIC</h1>
+            <p>SOON</p>
+          </Fade>
+        </div>
+        <div className="slider">
+          <h1>SOON SOON SOON SOON SOON SOON</h1>
+          <h1>SOON SOON SOON SOON SOON SOON</h1>
+        </div>
+      </main>
+    </section>
+  );
 };
 
 export default Music;
