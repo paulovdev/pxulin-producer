9440cc7c3fe217bab7829a1f8c2e218bf4546e8d
pxulin <fudevil123@gmail.com>
2023-08-19 22:57:09 -0300

    new

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.scss";
-import { BsFillArrowRightCircleFill } from "react-icons/bs";
+
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { Fade } from "react-awesome-reveal";
 import { Tooltip as ReactTooltip } from "react-tooltip";
@@ -10,30 +10,17 @@ const Home = () => {
     <section id="home">
       <main className="grid-layout">
         <div className="home-content">
-          <Fade cascade direction="down" duration={400} triggerOnce>
-            <h1>
-              Pxulin is a music artist who has uploaded songs, albums, and
-              playlists on SoundCloud.
-            </h1>
-          </Fade>
-
           <Fade
             cascade
             direction="down"
             duration={400}
-            delay={800}
-            triggerOnce
-          >
-            <a href="https://soundcloud.com/pxulin" target="__blank">
-              <div
-                className="button-container"
-                data-tooltip-id="my-tooltip-1"
-              >
-                <span>SOUNDCLOUD</span>
-                <BsFillArrowRightCircleFill size={32} />
-              </div>
-            </a>
+            triggerOnce>
+            <h1>
+              Pxulin is a music artist who has uploaded songs, albums, and
+              playlists on soundcloud.
+            </h1>
           </Fade>
+
         </div>
 
         <Fade
@@ -43,28 +30,22 @@ const Home = () => {
           delay={1200}
           triggerOnce
         >
-          <div className="botton-content">
-            <div className="arrow-text">
-              <p>© 2023 pxulin</p>
-              <a href="#gang">
-                <MdKeyboardArrowDown color="#fff" size={22} />
-              </a>
-            </div>
-
-            <span>
-              Pxulin's music can be streamed for free on SoundCloud, both on
-              desktop and mobile devices. The artist's profile on the platform
-              allows users to discover followers and stream tracks, albums,
-              and playlists
-            </span>
+          <div className="home-bottom-content">
+            <h1>© 2023 pxulin</h1>
+            <br />
+            <a href="https://soundcloud.com/pxulin/infernal-drum-kit" target="__blank" data-tooltip-id="my-tooltip-1">INFERNAL DRUM KIT OUT NOW!</a>
           </div>
         </Fade>
+
+        <a href="#gang" className="arrow">
+          <MdKeyboardArrowDown color="#fff" size={42} />
+        </a>
       </main>
 
       <ReactTooltip
         id="my-tooltip-1"
         place="bottom"
-        content="Go to my SoundCloud page?"
+        content="LISTEN TO THIS?"
       />
     </section>
   );
