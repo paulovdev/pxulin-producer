353448d5131faa4fd25737368d4b0d0766c3f5a2
pxulin <fudevil123@gmail.com>
2023-10-08 08:39:15 -0700

    new

--- src/AppLeader.jsx
+++ src/AppLeader.jsx
@@ -1,6 +1,6 @@
 import Home from './pages/home/Home';
 import About from './pages/about/About';
-import Works from './pages/works/works';
+import Works from './pages/works/Works';
 import ContactHome from './pages/contact/ContactHome';
 import Features from './pages/features/Features';
 import Several from './pages/Several/Several';
