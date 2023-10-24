5970b174fbeeda933c39b4b21d059445ad2bb465
pxulin <fudevil123@gmail.com>
2023-10-23 22:31:06 -0300

    new

--- src/utils/Loader/Loader.jsx
+++ src/utils/Loader/Loader.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import './Loader.scss'
+import React from "react";
+import { motion } from "framer-motion";
+import "./Loader.scss";
 
 const Loader = () => {
-    return (
-        <>
-            <motion.div
-                animate={{ y: '100%', background: "#000" }}
-                transition={{
-                    delay: 2,
-                    type: "spring",
-                }}
-                className="loader">
+  return (
+    <>
+      <motion.div
+        animate={{ y: "100%", background: "#000" }}
+        transition={{
+          delay: 2,
+          type: "spring",
+        }}
+        className="loader"
+      >
+        <motion.div
+          initial={{ y: "100%" }}
+          animate={{ y: "0", transition: 1 }}
+          className="loading"
+        >
+          <h1>PAULO</h1>
+        </motion.div>
+      </motion.div>
+    </>
+  );
+};
 
-                <motion.div
-                    initial={{ y: "100%" }}
-                    animate={{ y: "0", transition: 1 }}
-                    className='loading'
-                >
-                    <img src="/loading.gif" width={50} alt="" />
-                </motion.div>
-
-            </motion.div>
-        </>
-    )
-}
-
-export default Loader
+export default Loader;
